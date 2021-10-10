02db311918b0b8087e99b8e60fc2d1c8d5e3ab2b
szeeshanhassan3347897978 <47072013+szeeshanhassan3347897978@users.noreply.github.com>
2021-10-10 12:08:00 +0500

    Add files via upload

---
+++ WebsiteTemplate_Three layer/BusinessLayer/BusinessManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using BusinessLayer;
+using DataAccessLayer;
+
+namespace BusinessLayer
+{
+    public class BusinessManager
+    {
+        public string ConnectionString { get; set; }
+        private DBManager _dbManager;
+        public DBManager DBManager
+        {
+            get
+            {
+                return _dbManager;
+            }
+            set
+            {
+                _dbManager = value;
+            }
+        }
+
+        public BusinessManager()
+        {
+           
+        }
+
+        public List<Supplier> ManageSupplier(Supplier obj, OperationTypes operationType)
+        {
+            List<Supplier> result = new List<Supplier>();
+            switch (operationType)
+            {
+                case OperationTypes.NotDefined:                    
+                    break;
+                case OperationTypes.Add:
+                    obj.addSupplier();
+                    break;
+                case OperationTypes.Update:
+                    obj.updateSupplier();
+                    break;
+                case OperationTypes.Delete:
+                    obj.deleteSupplier();
+                    break;
+                case OperationTypes.GetAll:
+                   result = obj.GetAllSupplier();
+                    break;
+                case OperationTypes.GetSpecific:
+                   result = obj.GetSpecificSupplier();
+                    break;
+                default:
+                    break;
+
+            }
+            return result;
+
+        }
+
+        
+    }
+}
